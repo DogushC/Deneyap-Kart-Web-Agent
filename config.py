a45c8a613bb33071b6aa0f5f6e47549e4ce4e375
Taha Üzümcü <36675318+kinkintama@users.noreply.github.com>
2021-11-09 17:39:45 +0300

    version update

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import appdirs
 
 deneyapKart = "deneyap:esp32:dydk_mpv10"
 deneyapMini = "deneyap:esp32:dym_mpv10"
-version = "0.9.1"
+version = "0.9.2"
 
 TEMP_PATH = f"{appdirs.user_data_dir()}\DeneyapKartWeb\Temp"
 CONFIG_PATH = f"{appdirs.user_data_dir()}\DeneyapKartWeb"
